88e788a58cecc80c668e811c609320b681895f30
Paul Rupke <paul.rupke@gmail.com>
2020-10-27 20:13:32 -0400

    Add login route, add config refs

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, session, request, url_for, redirect, render_template, jsonify, flash, send_from_directory
+from oauth.oauth import *
+from random import randint
+
+import config
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/login', methods=['GET'])
 def index():
-	return "Hello world"
+  session['state'] = str(randint(1000000, 99999999))
+  response, success = getAccessToken(config.client_id, config.client_secret, config.redirect_uri, code)
+  test()
+  return "Hello world"
 
 if __name__== '__main__':
+  import credentials
+  app.secret_key = credentials.SECRET_KEY
+
+  # get Yahoo Oauth credentials
+  config.client_id = credentials.consumer_key
+  config.client_secret = credentials.consumer_secret
+  config.redirect_uri = "oob"
   app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,128 @@
+import os
+
+site = "https://slowdraft.herokuapp.com"
+teams = []
+league_data = []
+
+# DB variables
+conn = ''
+
+# Twilio settings (for SMSes)
+account_sid = ''
+auth_token = ''
+twilio = ''
+
+# number for sending test SMSes
+if 'test_number' in os.environ:
+	test_number = os.environ['test_number']
+else:
+	import credentials
+	test_number = credentials.test_number	
+
+# Sendgrid settings (for emails)
+mail_server = ''
+mail_port = 465
+mail_username = ''
+mail_password = ''
+mail_use_tls = False
+mail_use_ssl = True
+
+# NHL.com API endpoints and team IDs
+NEXT_GAME_URL = "https://statsapi.web.nhl.com/api/v1/teams/%s?expand=team.schedule.next"
+NHL_TEAM_ID = {
+    'New Jersey Devils' : '1',
+    'New York Islanders' : '2',
+    'New York Rangers' : '3',
+    'Philadelphia Flyers' : '4',
+    'Pittsburgh Penguins' : '5',
+    'Boston Bruins' : '6',
+    'Buffalo Sabres' : '7',
+    'Montreal Canadiens' : '8',
+    'Ottawa Senators' : '9',
+    'Toronto Maple Leafs' : '10',
+    'Carolina Hurricanes' : '12',
+    'Florida Panthers' : '13',
+    'Tampa Bay Lightning' : '14',
+    'Washington Capitals' : '15',
+    'Chicago Blackhawks' : '16',
+    'Detroit Red Wings' : '17',
+    'Nashville Predators' : '18',
+    'St. Louis Blues' : '19',
+    'Calgary Flames' : '20',
+    'Colorado Avalanche' : '21',
+    'Edmonton Oilers' : '22',
+    'Vancouver Canucks' : '23',
+    'Anaheim Ducks' : '24',
+    'Dallas Stars' : '25',
+    'Los Angeles Kings' : '26',
+    'San Jose Sharks' : '28',
+    'Columbus Blue Jackets' : '29',
+    'Minnesota Wild' : '30',
+    'Winnipeg Jets' : '52',
+    'Arizona Coyotes' : '53',
+    'Vegas Golden Knights' : '54'
+}
+
+# Yahoo endpoints 
+YAHOO_BASE_URL = "https://fantasysports.yahooapis.com/fantasy/v2/"
+GET_TOKEN_URL = "https://api.login.yahoo.com/oauth2/get_token" # used for Oauth
+
+# Yahoo Oauth variables - set from env variables at runtime
+client_id = ''
+client_secret = ''
+redirect_uri = ''
+league_key = ''
+team_id = ''
+
+# Yahoo stat_id mappings - can comment out ones that are not needed
+SKATER_STAT_INDEX = {
+	'careerGP' : 'career GP',
+	'0' : 'GP',
+	'1' : 'G',
+	'2' : 'A',
+	'3' : 'P',
+	'4' : '+/-',
+	'5' : 'PIM',
+	# '6' : 'PPG',
+	# '7' : 'PPA',
+	'8' : 'PPP',
+	# '9' : 'SHG?',
+	# '10' : 'SHA?',
+	# '11' : '',
+	# '12' : '',
+	# '13' : '',
+	'14' : 'SOG',
+	'15' : 'S%',
+	'16' : 'FW',
+	# '17' : 'FOL',
+	# '18' : 'Starts',
+	# '19' : 'W',
+	# '20' : '',
+	# '21' : '',
+	# '22' : 'GA',
+	# '23' : 'GAA',
+	# '24' : 'SA',
+	# '25' : 'SV',
+	# '26' : 'SV%',
+	# '27' : '',
+	# '28' : '',
+	# '29' : 'GP?',
+	# '30' : 'GP?',
+	'31' : 'HIT',
+	'32' : 'BLK'
+}
+
+GOALIE_STAT_INDEX = {
+	'careerGP' : 'career GP',
+	'18' : 'GS',
+	'19' : 'W',
+	# '20' : '',
+	# '21' : '',
+	'22' : 'GA',
+	'23' : 'GAA',
+	'24' : 'SA',
+	'25' : 'SV',
+	'26' : 'SV%',
+	# '27' : '',
+	# '28' : '',
+}
