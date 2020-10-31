c8339718092c14bf9aaf1ac4e174c7f6032c6099
Paul Rupke <paul.rupke@gmail.com>
2020-10-31 17:03:52 -0400

    Add static react build

---
+++ build/precache-manifest.8cb866d358d93ac7fc1fd3b082cb7244.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b567c5754652ef6a6e845c35e8f8e030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24e57a3c4653d52dc817",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "aef780ce1c230d39d6d9",
+    "url": "/static/css/main.5c152fe8.chunk.css"
+  },
+  {
+    "revision": "24e57a3c4653d52dc817",
+    "url": "/static/js/2.374ca857.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.374ca857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef780ce1c230d39d6d9",
+    "url": "/static/js/main.3dc714d3.chunk.js"
+  },
+  {
+    "revision": "c1ddc232552a0beffaa2",
+    "url": "/static/js/runtime-main.7ca357b8.js"
+  }
+]);
