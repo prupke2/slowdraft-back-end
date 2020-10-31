cb3a2126a8fe9b8e7e72674a19c5e556a3ceb0ee
Paul Rupke <paul.rupke@gmail.com>
2020-10-31 17:03:36 -0400

    Set app to run static index.html

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, session, request, url_for, redirect, render_template, jsonify, flash, send_from_directory
-from oauth.oauth import *
+from flask import Flask, request, send_from_directory, session, request, url_for, redirect, render_template, jsonify, flash, send_from_directory
+from oauth import *
 from random import randint
-
+import time
 import config
+import os, sys, json
 
-app = Flask(__name__)
+app = Flask(__name__, 
+  static_url_path='',
+  static_folder='build',
+  template_folder='build')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+  if path != "" and os.path.exists(app.static_folder + '/' + path):
+    return send_from_directory(app.static_folder, path)
+  else:
+    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/login', methods=['GET'])
 def index():
   session['state'] = str(randint(1000000, 99999999))
+  code = 12345
   response, success = getAccessToken(config.client_id, config.client_secret, config.redirect_uri, code)
   test()
   return "Hello world"
+
+@app.route('/test')
+def time():
+  response = {'test': 'test worked'}
+  return jsonify(response)
 
 if __name__== '__main__':
   import credentials
@@ -21,4 +39,8 @@ if __name__== '__main__':
   config.client_id = credentials.consumer_key
   config.client_secret = credentials.consumer_secret
   config.redirect_uri = "oob"
+  app.run(debug=True)
+else:
+  print("Not name = main")
+  app.secret_key = "asdf"
   app.run(debug=True)
