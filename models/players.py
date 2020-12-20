25bd1217f05f23061b941d47365bcbd65e4d89b9
Paul Rupke <paul.rupke@gmail.com>
2020-12-19 20:47:09 -0500

    update players model

--- models/players.py
+++ models/players.py
@@ -1,7 +1,7 @@
 from app import *
 from config import *
 import yahoo_api
-# from db import *
+import db
 
 # Form for searching players
 # class PlayerSearchForm(Form):
@@ -9,64 +9,32 @@ import yahoo_api
 # 	# position = SelectField('Position')
 # 	showdrafted = BooleanField('showdrafted')
 
-# def getDBPlayers(sortby, sortdir, position, player_search, offset):
-# 	database = db.DB()
-# 	sql_base = "SELECT * FROM yahoo_db_19 y "
-# 	exclude_taken_players = "WHERE NOT EXISTS (SELECT player_id FROM user_team ut WHERE ut.player_id = y.player_id) "
-# 	include_taken_players = "LEFT JOIN user_team ut ON ut.player_id = y.player_id LEFT JOIN users u ON u.user_id = ut.user_id"
-# 	# if (position == 'G' or position == 'goalie_prospects') and sortby not in GOALIE_STAT_INDEX:
-# 	# 	sortby = 19 
-# 	# if (position != 'G' and position != 'goalie_prospects') and sortby not in SKATER_STAT_INDEX:
-# 	# 	sortby = 3 	
-# 	if position != None or player_search != '':
-# 		if player_search != '':
-# 			where = " WHERE y.name LIKE '%" + str(player_search) + "%'"
-# 		else:
-# 			if position == 'LW,RW,C':
-# 				where = "AND ((position LIKE '%LW%') OR (position LIKE '%RW%') OR (position LIKE '%C%')) "
-# 			elif position == "LW,RW":
-# 				where = "AND ((position LIKE '%LW%') OR (position LIKE '%RW%')) "	
-# 			elif position == "prospects":
-# 				where = "AND position != 'G' AND prospect = 1"
-# 			elif position == "goalie_prospects":	
-# 				where = "AND position = 'G' AND prospect = 1"
-# 			elif position == "multi":
-# 				where = "AND (LENGTH(position) > 2)"	
-# 			else:
-# 				where = "AND position LIKE '%" + str(position) + "%' "
+def get_db_players(position, exclude_taken_players):
+	database = db.DB()
+	if position == "G":
+		query = "SELECT name, position, prospect, player_id, player_key, team, careerGP, `18`, " \
+					+ "`19`, `22`, CAST(`23` AS CHAR) AS `23`, `24`, `25`, `26` FROM yahoo_db_19 y "
+	else:
+		query = "SELECT * FROM yahoo_db_19 y "
+	if exclude_taken_players == True:
+		query += "WHERE NOT EXISTS (SELECT player_id FROM user_team ut WHERE ut.player_id = y.player_id) AND "
+	else:
+		query += "LEFT JOIN user_team ut ON ut.player_id = y.player_id LEFT JOIN users u ON u.user_id = ut.user_id WHERE "
 
-# 	else:
-# 		where = ""		
-# 	if sortby:
-# 		if sortdir:
-# 			if sortdir == '1':
-# 				sortdir = 'ASC'
-# 				if sortby == '23':
-# 					where += " AND `18` >= 10" # requires 10 games started when sorting goalies by GAA
-# 			else:
-# 				sortdir = 'DESC'	
-# 		else:
-# 			sortdir = 'DESC'	
-
-# 		order = " ORDER BY `" + str(sortby) + "` " + sortdir
-# 	else:
-# 		order = ""	
-# 	if player_search:
-# 		sql = sql_base + include_taken_players + where + order + " LIMIT 100"
-# 	else:
-# 		sql = sql_base + exclude_taken_players + where + order + " LIMIT 25 OFFSET " + str(offset)
-# 	print(str(sql))
-# 	result = database.cur.execute(sql)	
-# 	players = database.cur.fetchall()
-# 	# print(str(players))
-# 	skaters = []
-# 	goalies = []
-# 	for player in players:
-# 		if player['position'] == 'G':
-# 			goalies.append(player)
-# 		else:
-# 			skaters.append(player)	
-# 	return skaters, goalies
+	if position == "G":
+		query += "position = 'G' "
+	else:
+		query += "position != 'G' "
+	
+	print(str(query))
+	result = database.cur.execute(query)	
+	players = database.cur.fetchall()
+	player_array = []
+	for player in players:
+		player_array.append(player)
+	
+	print(f"player_array: {player_array}")
+	return players
 
 def get_players(sortby, sortdir, position, player_search, offset):
 	LEAGUE_URL = YAHOO_BASE_URL + "league/" + config.league_key
