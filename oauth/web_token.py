beb328c903a50488fe7f0ff32108c9aa6b39b6e2
Paul Rupke <paul.rupke@gmail.com>
2022-02-05 14:48:21 -0500

    add exception_handler and check_if_admin wrappers

--- oauth/web_token.py
+++ oauth/web_token.py
@@ -1,10 +1,13 @@
 import datetime
 import config
 import jwt
+from flask import request, jsonify
+from functools import wraps
+import util
 
 def generate_web_token(my_team, access_token, refresh_token):
+  # 3888000 seconds = 45 days
   expiry = datetime.datetime.now() + datetime.timedelta(seconds=3888000)
-  # 3888000 # expires 45 days in the future
   payload = {
     "iss": "SlowDraft",
     "access_token": access_token,
@@ -17,12 +20,46 @@ def generate_web_token(my_team, access_token, refresh_token):
     "team_name": my_team['team_name'],
     "color": my_team['color']
   }
-  print(f"payload: {payload}")
   return jwt.encode(payload, config.client_secret, algorithm="HS256")
 
 def decode_web_token(token):
-  try: 
-    return jwt.decode(token, config.client_secret, issuer="SlowDraft", algorithms="HS256")
-  except jwt.ExpiredSignatureError:
-    print("web token expired")
-    return None
+  return jwt.decode(token, config.client_secret, issuer="SlowDraft", algorithms="HS256")
+
+def exception_handler(func):
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+    try:
+      user = decode_web_token(request.headers['Authorization'])
+    except jwt.ExpiredSignatureError:
+      print("Web token expired", 401)
+      return util.return_error('expired_token')
+    except (jwt.InvalidTokenError, KeyError):
+      print("Missing or invalid token.")
+      return util.return_error('invalid_token', 403)
+    except Exception as e:
+      print(f"Error decoding token: {e}")
+      return util.return_error('invalid_token', 403)
+
+    try:
+      return func(user, *args, **kwargs)
+    except Exception as e:
+      print(f"Error in {str(func)}: {e}")
+      return util.return_error('invalid_token', 403)
+    
+  wrapper.__name__ = func.__name__
+  return wrapper
+
+def check_if_admin(func):
+  def wrapper(user):
+    try:
+      print(f"user: {user}")
+      if user['role'] != 'admin':
+        return util.return_error('not_authorized', 403)
+      else:
+        response = func(user)
+        return response
+    except Exception as e:
+      print(f"Error in: {str(func)}: {e}")
+      return util.return_error('unknown_error', 400)
+  wrapper.__name__ = func.__name__
+  return wrapper
