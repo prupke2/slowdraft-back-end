8dde28ad3ebd014a181b6a7a3a37c62601f0748c
Paul Rupke <paul.rupke@gmail.com>
2022-02-13 20:39:49 -0500

    add update util

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 from flask import jsonify
+import db
+import datetime
 
 def return_true():
   return jsonify({ "success": True })
@@ -11,3 +13,13 @@ def return_error(message, status = 400):
       "status": status
     }
   )
+
+def update(table, draft_id):
+  database = db.DB()
+  query = f""" UPDATE updates 
+		SET {table} = %s
+		WHERE draft_id = %s
+	"""
+  database.cur.execute(query, (datetime.datetime.utcnow(), draft_id))
+  database.connection.commit()
+  return
