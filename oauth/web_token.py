372318dbd1ba398fe9bfce168577e1736ba0b30d
Paul Rupke <paul.rupke@gmail.com>
2022-04-11 23:29:35 -0400

    generate_temp_web_token

--- oauth/web_token.py
+++ oauth/web_token.py
@@ -23,6 +23,35 @@ def generate_web_token(my_team, access_token, refresh_token):
   }
   return jwt.encode(payload, config.client_secret, algorithm="HS256")
 
+def generate_temp_web_token(my_team, access_token, refresh_token):
+  # 3888000 seconds = 45 days
+  expiry = datetime.datetime.now() + datetime.timedelta(seconds=3888000)
+  payload = {
+    "iss": "SlowDraft",
+    "access_token": access_token,
+    "refresh_token": refresh_token,
+    "exp": expiry,
+    "draft_id": None,
+    "yahoo_league_id": my_team['yahoo_league_id'],
+    "yahoo_team_id": my_team['yahoo_team_id'],
+    "team_key": my_team['team_key'],
+    "team_name": my_team['team_name'],
+    "role": 'admin',
+    "temp": True
+  }
+  return jwt.encode(payload, config.client_secret, algorithm="HS256")
+
+def replace_temp_token_with_web_token(user, draft_id, color_codes):
+  new_user_data = {}
+  new_user_data['draft_id'] = draft_id
+  new_user_data['yahoo_league_id'] = user['yahoo_league_id']
+  new_user_data['yahoo_team_id'] = user['yahoo_team_id']
+  new_user_data['team_key'] = user['team_key']
+  new_user_data['team_name'] = user['team_name']
+  new_user_data['role'] = 'admin'
+  new_user_data['color'] = color_codes[int(user['yahoo_team_id'])]
+  return generate_web_token(new_user_data, user['access_token'], user['refresh_token'])
+
 def decode_web_token(token):
   return jwt.decode(token, config.client_secret, issuer="SlowDraft", algorithms="HS256")
 
