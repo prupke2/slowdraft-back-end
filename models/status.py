efc75c40b68bd2d3b9989c849b489c4339955e14
Paul Rupke <paul.rupke@gmail.com>
2022-04-15 23:35:12 -0400

    update status functions

--- models/status.py
+++ models/status.py
@@ -7,84 +7,86 @@ import yahoo_api
 import db
 
 def get_updates_with_league(yahoo_league_id, team_key):
-	database = db.DB()
-	database.cur.execute("SELECT * FROM updates WHERE yahoo_league_id = %s", yahoo_league_id)
-	updates = database.cur.fetchone()
-	return jsonify({'updates': updates, 'drafting_now': check_if_drafting(database, team_key)})
+    database = db.DB()
+    database.cur.execute(
+        "SELECT * FROM updates WHERE yahoo_league_id = %s", yahoo_league_id)
+    updates = database.cur.fetchone()
+    return jsonify({'success': True, 'updates': updates, 'drafting_now': check_if_drafting(database, team_key)})
 
 def check_if_drafting(database, team_key):
-	sql = "SELECT drafting_now FROM users WHERE team_key = %s"
-	database.cur.execute(sql, team_key)
-	result = database.cur.fetchone()
-	if result and result['drafting_now'] == 1:
-		return True
-	return False
-	
+    sql = "SELECT drafting_now FROM users WHERE team_key = %s"
+    database.cur.execute(sql, team_key)
+    result = database.cur.fetchone()
+    if result and result['drafting_now'] == 1:
+        return True
+    return False
+
 # Makes sure the draft session variable is set
 def check_league(f):
-	@wraps(f)
-	def wrap(*args, **kwargs):
-		if 'draft_id' not in session:
-			database = db.DB()
-			sql = "SELECT * FROM league WHERE yahoo_league_id = %s"
-			league_id = str(config.league_key[-5:])
-			result = database.cur.execute(sql, league_id)
-			league = database.cur.fetchone()
-			session['draft_id'] = league['most_recent_draft_id']
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'draft_id' not in session:
+            database = db.DB()
+            sql = "SELECT * FROM league WHERE yahoo_league_id = %s"
+            league_id = str(config.league_key[-5:])
+            result = database.cur.execute(sql, league_id)
+            league = database.cur.fetchone()
+            session['draft_id'] = league['most_recent_draft_id']
 
-		return f(*args, **kwargs)
-	return wrap		
+        return f(*args, **kwargs)
+    return wrap
 
-def set_team_sessions(access_token, refresh_token):
-	my_team_data = {}
-	TEAM_URL = config.YAHOO_BASE_URL + "league/" + config.league_key + "/teams"
-	team_query = yahoo_api.yahoo_request(TEAM_URL)
-	print(f"team_query: {team_query}")
-	my_team_data['yahoo_league_id'] = team_query['fantasy_content']['league']['league_id']
-	teams = []
-	for team in team_query['fantasy_content']['league']['teams']['team']:
-		team_data = {}
-		# print("TEAM: " + str(team))
-		team_data['yahoo_team_id'] = int(team['team_id'])
-		team_data['team_key'] = team['team_key']
-		team_data['user'] = team['managers']['manager']['nickname']
-		team_data['user_logo'] = team['managers']['manager']['image_url']
-		team_data['team_name'] = team['name']
-		team_data['team_logo'] = team['team_logos']['team_logo']['url']
-		# team_data['waiver_priority'] = team['waiver_priority']
 
-		# # some managers choose not to share their email, so this sets it to empty if that is the case
-		try:
-			team_data['email'] = team['managers']['manager']['email']
-		except:
-			team_data['email'] = ""	
+def set_team_sessions(league_key, team_query):
+    my_team_data = {}
 
-		if 'is_owned_by_current_login' in team:
-		# if session['guid'] == team['managers']['manager']['guid']:
-			print(f"team: {team}")
-			my_team_data['yahoo_team_id'] = int(team['team_id'])
-			my_team_data['logo'] = team['team_logos']['team_logo']['url']
-			my_team_data['team_name'] = team['name']
-			my_team_data['team_key'] = team['team_key']
+    my_team_data['yahoo_league_id'] = team_query['fantasy_content']['league']['league_id']
+    teams = []
+    for team in team_query['fantasy_content']['league']['teams']['team']:
+        team_data = {}
+        # print("TEAM: " + str(team))
+        team_data['yahoo_team_id'] = int(team['team_id'])
+        team_data['team_key'] = team['team_key']
+        team_data['user'] = team['managers']['manager']['nickname']
+        team_data['user_logo'] = team['managers']['manager']['image_url']
+        team_data['team_name'] = team['name']
+        team_data['team_logo'] = team['team_logos']['team_logo']['url']
+        # team_data['waiver_priority'] = team['waiver_priority']
 
-			data = get_user(my_team_data['team_key'], my_team_data['yahoo_league_id'])
-			if data == None:
-				is_live = False
-				registered = False
-				my_team_data['role'] = 'admin'
-			else:
-				my_team_data['role'] = data['role']
-				my_team_data['color'] = data['color']
-				my_team_data['draft_id'] = data['draft_id']
-				my_team_data['current_pick'] = data['current_pick']
-				is_live = data['is_live']
-				registered = True
-		teams.append(team_data)
-	return teams, my_team_data, is_live, registered
+        # # some managers choose not to share their email, so this sets it to empty if that is the case
+        try:
+            team_data['email'] = team['managers']['manager']['email']
+        except:
+            team_data['email'] = ""
+
+        if 'is_owned_by_current_login' in team:
+            # if session['guid'] == team['managers']['manager']['guid']:
+            print(f"team: {team}")
+            my_team_data['yahoo_team_id'] = int(team['team_id'])
+            my_team_data['logo'] = team['team_logos']['team_logo']['url']
+            my_team_data['team_name'] = team['name']
+            my_team_data['team_key'] = team['team_key']
+
+            data = get_user(my_team_data['team_key'],
+                            my_team_data['yahoo_league_id'])
+            if data == None:
+                is_live = False
+                registered = False
+                my_team_data['role'] = 'admin'
+            else:
+                my_team_data['role'] = data['role']
+                my_team_data['color'] = data['color']
+                my_team_data['draft_id'] = data['draft_id']
+                my_team_data['current_pick'] = data['current_pick']
+                is_live = data['is_live']
+                registered = True
+        teams.append(team_data)
+    return teams, my_team_data, is_live, registered
+
 
 def get_user(team_key, yahoo_league_id):
-	database = db.DB()
-	sql = """
+    database = db.DB()
+    sql = """
 		SELECT u.role, u.color, d.draft_id, d.current_pick, d.is_live
 		FROM users u
 		INNER JOIN draft d
@@ -92,35 +94,37 @@ def get_user(team_key, yahoo_league_id):
 		WHERE team_key = %s
 		AND d.yahoo_league_id = %s
 	"""
-	database.cur.execute(sql, (team_key, yahoo_league_id))
-	return database.cur.fetchone()
+    database.cur.execute(sql, (team_key, yahoo_league_id))
+    return database.cur.fetchone()
+
 
 def check_draft_status(f):
-	@wraps(f)
-	def wrap(*args, **kwargs):
-		database = db.DB()
-		# draft.checkCurrentPickInDraft()
-		sql = "SELECT * FROM draft d LEFT JOIN draft_picks dp ON d.draft_id = dp.draft_id WHERE d.draft_id = %s ORDER BY dp.overall_pick"
-		database.cur.execute(sql, session['draft_id'])
-		print("DRAFT ID: " + str(session['draft_id']))
-		draft_info = database.cur.fetchone()
-		print("info: \n" + str(draft_info))
-		if draft_info['is_live'] == 1:
-			session['current_pick'] = draft.getCurrentPickInfo(draft_info['current_pick'])
-		else:
-			session['current_pick'] = 0	
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        database = db.DB()
+        # draft.checkCurrentPickInDraft()
+        sql = "SELECT * FROM draft d LEFT JOIN draft_picks dp ON d.draft_id = dp.draft_id WHERE d.draft_id = %s ORDER BY dp.overall_pick"
+        database.cur.execute(sql, session['draft_id'])
+        print("DRAFT ID: " + str(session['draft_id']))
+        draft_info = database.cur.fetchone()
+        print("info: \n" + str(draft_info))
+        if draft_info['is_live'] == 1:
+            session['current_pick'] = draft.getCurrentPickInfo(
+                draft_info['current_pick'])
+        else:
+            session['current_pick'] = 0
 
-		draft_start_time = draft_info['draft_start_time_utc']
-		if (draft_info['is_live'] == 0) and (draft_start_time < datetime.datetime.utcnow() and draft_info['is_over'] == 0):
-			sql = "UPDATE draft SET is_live=1, current_pick = 1 WHERE draft_id = %s"
-			database.cur.execute(sql, session['draft_id'])
-			database.connection.commit()
-			sql = "UPDATE users SET drafting_now = 1 WHERE user_id = %s"
-			print("USER ID: " + str(draft_info['user_id']))
-			database.cur.execute(sql, draft_info['user_id'])
-			database.connection.commit()
-		# print("IS OVER? : " + str(draft_info['is_over']))	
-		if draft_info['is_over'] == 1:
-			session['current_pick'] = None
-		return f(*args, **kwargs)
-	return wrap		
+        draft_start_time = draft_info['draft_start_time_utc']
+        if (draft_info['is_live'] == 0) and (draft_start_time < datetime.datetime.utcnow() and draft_info['is_over'] == 0):
+            sql = "UPDATE draft SET is_live=1, current_pick = 1 WHERE draft_id = %s"
+            database.cur.execute(sql, session['draft_id'])
+            database.connection.commit()
+            sql = "UPDATE users SET drafting_now = 1 WHERE user_id = %s"
+            print("USER ID: " + str(draft_info['user_id']))
+            database.cur.execute(sql, draft_info['user_id'])
+            database.connection.commit()
+        # print("IS OVER? : " + str(draft_info['is_over']))
+        if draft_info['is_over'] == 1:
+            session['current_pick'] = None
+        return f(*args, **kwargs)
+    return wrap
